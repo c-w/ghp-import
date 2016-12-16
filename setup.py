d767fbe75b6162a08a08ee404ca6cbb306460449
Paul J. Davis <paul.joseph.davis@gmail.com>
2016-12-16 11:43:40 -0600

    This is version 0.5.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ LONG_DESC = io.open(LONG_DESC_PATH, encoding = "utf-8").read()
 
 setup(
     name = "ghp-import",
-    version = "0.4.2",
+    version = "0.5.0",
     description = "Copy your docs directly to the gh-pages branch.",
     long_description = LONG_DESC,
     author = "Paul Joseph Davis",
