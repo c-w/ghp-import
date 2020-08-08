3bebda95b76b6c31114ba5e1882f7fb7dd5a3697
Clemens Wolff <clewolff@microsoft.com>
2020-08-08 15:35:43 -0400

    Properly close file objects

--- docs/build.py
+++ docs/build.py
@@ -10,10 +10,12 @@ def main():
     base = os.path.abspath(os.path.dirname(__file__))
 
     readme_path = os.path.join(os.path.dirname(base), "README.md")
-    readme = io.open(readme_path, encoding="utf-8").read()
+    with io.open(readme_path, encoding="utf-8") as fobj:
+        readme = fobj.read()
 
     template_path = os.path.join(base, "index.html.tmpl")
-    template = io.open(template_path, encoding="utf-8").read()
+    with io.open(template_path, encoding="utf-8") as fobj:
+        template = fobj.read()
 
     index_path = os.path.join(base, "index.html")
     with io.open(index_path, mode="w", encoding="utf-8") as fobj:
